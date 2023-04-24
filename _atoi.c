2e7e3eafe48099168f6dd3b95251758360fb20e6
DennisMatenge <mateshdennis@gmail.com>
2023-04-24 04:30:22 -0700

    _atoi.c

---
+++ _atoi.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - checks if character is a delimiter
+ * @c: the char to check
+ * @delim: the delimiter string
+ *
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+	for (int i = 0; delim[i] != '\0'; i++)
+	{
+		if (delim[i] == c)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: The character to input
+ *
+ * Return: 1 if c is alphabetic, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ *
+ * Return: 0 if no numbers in string, converted number otherwise
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+		{
+			sign = -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			result = result * 10 + (*s - '0');
+		}
+		else if (result > 0)
+		{
+			break;
+		}
+		s++;
+	}
+
+	return (sign * result);
+}
+
