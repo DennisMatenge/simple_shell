1d1f37e425c9a56e37d53fb41bb425e05c6d4361
Boldolsky <boldab24@gmail.com>
2023-04-28 14:06:30 -0700

    update

--- getinfo.c
+++ getinfo.c
@@ -1,3 +1,18 @@
+/*
+ * File: getinfo.c
+ * Authors: Jerry Doubles & Ediongsenyene Inokotong
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
 #include "shell.h"
 
 /**
@@ -23,23 +38,22 @@ void set_info(info_t *info, char **av)
 
 	info->fname = av[0];
 	if (info->arg)
+{
+	info->argv = strtow(info->arg, " \t");
+	if (!info->argv)
 	{
-		info->argv = strtow(info->arg, " \t");
-		if (!info->argv)
-		{
-			info->argv = malloc(sizeof(char *) * 2);
-			if (info->argv)
-			{
-				info->argv[0] = _strdup(info->arg);
-				info->argv[1] = NULL;
-			}
-		}
-		for (i = 0; info->argv && info->argv[i]; i++)
-			;
-		info->argc = i;
-
-		replace_alias(info);
-		replace_vars(info);
+	info->argv = malloc(sizeof(char *) * 2);
+	if (info->argv)
+	{
+	info->argv[0] = _strdup(info->arg);
+	info->argv[1] = NULL;
+	}
+	}
+	for (i = 0; info->argv && info->argv[i]; i++)
+	;
+	info->argc = i;
+	replace_alias(info);
+	replace_vars(info);
 	}
 }
 
@@ -55,20 +69,19 @@ void free_info(info_t *info, int all)
 	info->path = NULL;
 	if (all)
 	{
-		if (!info->cmd_buf)
-			free(info->arg);
-		if (info->env)
-			free_list(&(info->env));
-		if (info->history)
-			free_list(&(info->history));
-		if (info->alias)
-			free_list(&(info->alias));
-		ffree(info->environ);
-			info->environ = NULL;
-		bfree((void **)info->cmd_buf);
-		if (info->readfd > 2)
-			close(info->readfd);
-		_putchar(BUF_FLUSH);
+	if (!info->cmd_buf)
+	free(info->arg);
+	if (info->env)
+	free_list(&(info->env));
+	if (info->history)
+	free_list(&(info->history));
+	if (info->alias)
+	free_list(&(info->alias));
+	ffree(info->environ);
+	info->environ = NULL;
+	bfree((void **)info->cmd_buf);
+	if (info->readfd > 2)
+	close(info->readfd);
+	_putchar(BUF_FLUSH);
 	}
 }
-
